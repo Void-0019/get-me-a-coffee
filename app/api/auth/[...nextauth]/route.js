a0ac6ca69fd315135207f8f31e3cda571e243f32
Rishabh <rrishabhsingh94@gmail.com>
2026-08-04 01:16:17 +0530

    required updates

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
+import mongoose from 'mongoose'
+import User from "@/models/User"
+// import Payment from "@models/Payment.js"
 
 const authoptions = NextAuth({
     providers: [
@@ -8,7 +11,41 @@ const authoptions = NextAuth({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
-    ]
+    ],
+
+callbacks: {
+  async signIn({ user, account}) {
+    if (account.provider === 'github') {
+        // connect to the database
+        await mongoose.connect("mongodb://localhost:27017/coffee")
+
+        // check if the user already exists in database
+        const currentUser = await User.findOne({email: user.email})
+
+        if(!currentUser){
+            // Create a new user
+            const newUser = new User({
+                email: user.email,
+                username: user.email.split("@")[0],
+            })
+            await newUser.save();
+            user.name = newUser.username
+        }
+        else{
+            user.name = currentUser.username
+        }
+    }
+    return true;
+  },
+  async session({session, user, token}){
+    const dbUser = await User.findOne({email: session.user.email})
+
+    if(dbUser){
+    session.user.name = dbUser.username
+    }
+    return session
+  },
+}
 })
 
 export { authoptions as GET, authoptions as POST }
