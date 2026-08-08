afb3f3ebba3257a9f1c4fe06f59e672265351b79
Rishabh <rrishabhsingh94@gmail.com>
2026-08-08 18:47:49 +0530

    minor updates

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -6,7 +6,11 @@ import { useSession } from 'next-auth/react'
 
 const PaymentPage = ({ username }) => {
     const { data: session } = useSession();
-    const [paymentform, setPaymentform] = useState({})
+    const [paymentform, setPaymentform] = useState({
+        name: "",
+        message: "",
+        amount: "",
+    })
 
     const handleChange = (e) =>{
         setPaymentform({...paymentform, [e.target.name]: e.target.value})
@@ -17,14 +21,14 @@ const PaymentPage = ({ username }) => {
         let a = await initiate(amount, username, paymentform)
         let orderId = a.id
         var options = {
-            "key": process.env.KEY_ID, // Enter the Key ID generated from the Dashboard
+            "key": process.env.NEXT_PUBLIC_KEY_ID, // Enter the Key ID generated from the Dashboard
             "amount": amount, // Amount is in currency subunits. 
             "currency": "INR",
             "name": "Get Me A Coffee", //your business name
             "description": "Test Transaction",
             "image": "https://example.com/your_logo",
             "order_id": orderId, // This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            "callback_url": `${process.env.NEXTAUTH_URL}/api/razorpay`,
+            "callback_url": `${process.env.NEXT_PUBLIC_URL}/api/razorpay`,
             "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
                 "name": "Gaurav Kumar", //your customer's name
                 "email": "gaurav.kumar@example.com",
