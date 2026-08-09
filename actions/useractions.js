dcd9aa12d414ff0a681fe0f3545559d0078fa82f
Rishabh <rrishabhsingh94@gmail.com>
2026-08-10 01:17:13 +0530

    added profile updation

--- actions/useractions.js
+++ actions/useractions.js
@@ -29,28 +29,42 @@ export const initiate = async (amount, to_username, paymentform) => {
     let x = await instance.orders.create(options)
 
     // create a payment object which shows a pending payment in the database
-    await Payment.create({oid: x.id, amount: amount, to_user: to_username,name: paymentform.name, message: paymentform.message})
+    await Payment.create({ oid: x.id, amount: amount, to_user: to_username, name: paymentform.name, message: paymentform.message })
 
     return x
 }
 
-export const fetchuser = async (username) =>{
+export const fetchuser = async (username) => {
     await connectDB()
-    let u = await User.findOne({username: username})
-    let user = u.toObject({flattenObjectIds: true})
+    let u = await User.findOne({ username: username })
+    let user = u.toObject({ flattenObjectIds: true })
     return user
 }
 
-export const fetchpayments = async (username) =>{
+export const fetchpayments = async (username) => {
     await connectDB()
 
     // find all payments sorted by decreasing order of amount and flatten object Ids
-    
     let p = await Payment.find({ to_user: username, done: true })
         .sort({ amount: -1 })
         .lean()
-        return p.map(payment =>({
-            ...payment,
-            _id: payment._id.toString(),
-        }))
+    return p.map(payment => ({
+        ...payment,
+        _id: payment._id.toString(),
+    }))
+}
+
+export const updateProfile = async (data, oldusername) => {
+    await connectDB()
+    let ndata = Object.fromEntries(data)
+
+    // If the username is being updated, check if the username is available
+    if (oldusername !== ndata.username) {
+        let u = await User.findOne({ username: ndata.username })
+        if (u) {
+            return { error: "Username already exists" }
+        }
+    }
+
+    await User.updateOne({email: ndata.email}, ndata)
 }
