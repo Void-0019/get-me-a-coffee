6d71fd431e9d16019554c05d3d0371f5b0eb68a7
Rishabh <rrishabhsingh94@gmail.com>
2026-08-12 02:47:04 +0530

    fix: return 404 for non-existent usernames

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,8 +1,16 @@
 import React from "react";
 import PaymentPage from "@/components/PaymentPage";
+import { notFound } from "next/navigation";
+import { fetchuser } from "@/actions/useractions";
 
 const Username = async ({ params }) => {
   const { username } = await params;
+
+  //  Check if the username is inot present in the database, show a 404 page
+  const user = await fetchuser(username)
+  if (!user) {
+    return notFound()
+  }
 
   return <PaymentPage username={username} />;
 };
